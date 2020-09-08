c86109d89ec2f26312448792d5a968ddd18db8d4
Mark Cramer <mecramer@users.noreply.github.com>
2020-09-08 12:12:33 -0400

    Removed unused validation function, added getFieldProps to replace boilerplate onChange, onBlur and value props

--- src/components/YoutubeForm.js
+++ src/components/YoutubeForm.js
@@ -26,29 +26,6 @@ function YoutubeForm () {
         initialValues,
         onSubmit,
         validationSchema,
-        // formiks validate function which automatically receives the form's values as input
-        // 1.) the function must return an object
-        // 2.) the keys of the object is that of the name attribute for the form field
-        // 3.) a string value, indicating what the error is, should be assigned to the key
-        // validate      : (values) => {
-        //     // values.name, values.email, values.channel
-        //     // errors.name, errors.email, errors.channel
-        //     // errors.name = 'This field is required.'
-        //     let errors = {}
-        //     if (!values.name) {
-        //         errors.name = 'Required'
-        //     }
-        //     if (!values.email) {
-        //         errors.email = 'Required'
-        //     } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-        //         errors.email = 'Invalid email format.'
-        //     }
-        //     if (!values.channel) {
-        //         errors.channel = 'Required'
-        //     }
-
-        //     return errors
-        // },
     })
 
     // console.log('Form values', formik.values)
@@ -69,9 +46,7 @@ function YoutubeForm () {
                         type='text'
                         id='name'
                         name='name'
-                        onChange={formik.handleChange}
-                        onBlur={formik.handleBlur} // this formik built in function handles the touched object
-                        value={formik.values.name}
+                        {...formik.getFieldProps('name')} // this replaces onChange, onBlur and value props
                     />
                     {/* show error message if error and the field has been visited (touched) */}
                     {formik.touched.name && formik.errors.name ? (
@@ -85,9 +60,7 @@ function YoutubeForm () {
                         type='email'
                         id='email'
                         name='email'
-                        onChange={formik.handleChange}
-                        onBlur={formik.handleBlur}
-                        value={formik.values.email}
+                        {...formik.getFieldProps('email')}
                     />
                     {formik.touched.email && formik.errors.email ? (
                         <div className='error'>{formik.errors.email}</div>
@@ -100,9 +73,7 @@ function YoutubeForm () {
                         type='text'
                         id='channel'
                         name='channel'
-                        onChange={formik.handleChange}
-                        onBlur={formik.handleBlur}
-                        value={formik.values.channel}
+                        {...formik.getFieldProps('channel')}
                     />
                     {formik.touched.channel && formik.errors.channel ? (
                         <div className='error'>{formik.errors.channel}</div>
