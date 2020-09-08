fee1045dc3606e8e9e24fc7ddfb76dedd6310e76
Mark Cramer <mecramer@users.noreply.github.com>
2020-09-08 11:28:05 -0400

    Add validation
    Add validation to Formik through touch and the Formik built-in onBlur event function.

--- src/components/YoutubeForm.js
+++ src/components/YoutubeForm.js
@@ -6,43 +6,95 @@ function YoutubeForm () {
         // the properties in initialValues correspond to the name properties in the form fields
         initialValues : {
             name    : 'Mark',
-            email   : 'mecramer@yahoo.com',
-            channel : 'React',
+            email   : '',
+            channel : '',
         },
         // formiks onSubmit receives the values of the form as its argument
         onSubmit      : (values) => {
             console.log('Form data', values)
         },
+        // formiks validate function which automatically receives the form's values as input
+        // 1.) the function must return an object
+        // 2.) the keys of the object is that of the name attribute for the form field
+        // 3.) a string value, indicating what the error is, should be assigned to the key
+        validate      : (values) => {
+            // values.name, values.email, values.channel
+            // errors.name, errors.email, errors.channel
+            // errors.name = 'This field is required.'
+            let errors = {}
+            if (!values.name) {
+                errors.name = 'Required'
+            }
+            if (!values.email) {
+                errors.email = 'Required'
+            } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+                errors.email = 'Invalid email format.'
+            }
+            if (!values.channel) {
+                errors.channel = 'Required'
+            }
+
+            return errors
+        },
     })
 
     // console.log('Form values', formik.values)
+    // console.log('Form errors', formik.errors)
+
+    // the touched object gives you information on whether a field has been visited or not
+    console.log('Visited fields', formik.touched)
 
     return (
         <div>
             {/* Formik provides a submission helper function, handleSubmit */}
             <form onSubmit={formik.handleSubmit}>
-                <label htmlFor='name'>Name</label>
-                {/* formik.handleChange is formik's handler to change the form values, formik.values is the initial values */}
-                {/* this automatically calls the onSubmit method in the useFormik hook */}
-                <input type='text' id='name' name='name' onChange={formik.handleChange} value={formik.values.name} />
+                <div className='form-control'>
+                    <label htmlFor='name'>Name</label>
+                    {/* formik.handleChange is formik's handler to change the form values, formik.values is the initial values */}
+                    {/* this automatically calls the onSubmit method in the useFormik hook */}
+                    <input
+                        type='text'
+                        id='name'
+                        name='name'
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur} // this formik built in function handles the touched object
+                        value={formik.values.name}
+                    />
+                    {/* show error message if error and the field has been visited (touched) */}
+                    {formik.touched.name && formik.errors.name ? (
+                        <div className='error'>{formik.errors.name}</div>
+                    ) : null}
+                </div>
 
-                <label htmlFor='email'>E-mail</label>
-                <input
-                    type='email'
-                    id='email'
-                    name='email'
-                    onChange={formik.handleChange}
-                    value={formik.values.email}
-                />
+                <div className='form-control'>
+                    <label htmlFor='email'>E-mail</label>
+                    <input
+                        type='email'
+                        id='email'
+                        name='email'
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                        value={formik.values.email}
+                    />
+                    {formik.touched.email && formik.errors.email ? (
+                        <div className='error'>{formik.errors.email}</div>
+                    ) : null}
+                </div>
 
-                <label htmlFor='channel'>Channel</label>
-                <input
-                    type='text'
-                    id='channel'
-                    name='channel'
-                    onChange={formik.handleChange}
-                    value={formik.values.channel}
-                />
+                <div className='form-control'>
+                    <label htmlFor='channel'>Channel</label>
+                    <input
+                        type='text'
+                        id='channel'
+                        name='channel'
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                        value={formik.values.channel}
+                    />
+                    {formik.touched.channel && formik.errors.channel ? (
+                        <div className='error'>{formik.errors.channel}</div>
+                    ) : null}
+                </div>
 
                 <button type='submit'>Submit</button>
             </form>
