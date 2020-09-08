67b2259d9bafb3600ecf88621cb156c8afdb2368
Mark Cramer <mecramer@users.noreply.github.com>
2020-09-08 14:13:15 -0400

    Refactored with Formik components
    Add the Formik, Form, Field and ErrorMessage components to have Formik do a lot of the code creation work.

--- src/components/YoutubeForm.js
+++ src/components/YoutubeForm.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useFormik } from 'formik' // the hook (function) formik provides
+import { Formik, Form, Field, ErrorMessage } from 'formik'
 import * as Yup from 'yup' // yup library is used with Formik for form validation
 
 // the properties in initialValues correspond to the name properties in the form fields
@@ -22,67 +22,35 @@ const validationSchema = Yup.object({
 })
 
 function YoutubeForm () {
-    const formik = useFormik({
-        initialValues,
-        onSubmit,
-        validationSchema,
-    })
-
-    // console.log('Form values', formik.values)
-    // console.log('Form errors', formik.errors)
-
-    // the touched object gives you information on whether a field has been visited or not
-    console.log('Visited fields', formik.touched)
-
     return (
-        <div>
+        <Formik
+            initialValues={initialValues}
+            validationSchema={validationSchema}
+            onSubmit={onSubmit}
+        >
             {/* Formik provides a submission helper function, handleSubmit */}
-            <form onSubmit={formik.handleSubmit}>
+            <Form>
                 <div className='form-control'>
                     <label htmlFor='name'>Name</label>
-                    {/* formik.handleChange is formik's handler to change the form values, formik.values is the initial values */}
-                    {/* this automatically calls the onSubmit method in the useFormik hook */}
-                    <input
-                        type='text'
-                        id='name'
-                        name='name'
-                        {...formik.getFieldProps('name')} // this replaces onChange, onBlur and value props
-                    />
-                    {/* show error message if error and the field has been visited (touched) */}
-                    {formik.touched.name && formik.errors.name ? (
-                        <div className='error'>{formik.errors.name}</div>
-                    ) : null}
+                    <Field type='text' id='name' name='name' />
+                    <ErrorMessage name='name' />
                 </div>
 
                 <div className='form-control'>
                     <label htmlFor='email'>E-mail</label>
-                    <input
-                        type='email'
-                        id='email'
-                        name='email'
-                        {...formik.getFieldProps('email')}
-                    />
-                    {formik.touched.email && formik.errors.email ? (
-                        <div className='error'>{formik.errors.email}</div>
-                    ) : null}
+                    <Field type='email' id='email' name='email' />
+                    <ErrorMessage name='email' />
                 </div>
 
                 <div className='form-control'>
                     <label htmlFor='channel'>Channel</label>
-                    <input
-                        type='text'
-                        id='channel'
-                        name='channel'
-                        {...formik.getFieldProps('channel')}
-                    />
-                    {formik.touched.channel && formik.errors.channel ? (
-                        <div className='error'>{formik.errors.channel}</div>
-                    ) : null}
+                    <Field type='text' id='channel' name='channel' />
+                    <ErrorMessage name='channel' />
                 </div>
 
                 <button type='submit'>Submit</button>
-            </form>
-        </div>
+            </Form>
+        </Formik>
     )
 }
 
