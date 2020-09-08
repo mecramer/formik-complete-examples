f924fcc93a8401bb9fc1c99dc3e9f84c83c3c327
Mark Cramer <mecramer@users.noreply.github.com>
2020-09-08 15:39:08 -0400

    Added nested objects and arrays examples

--- src/components/YoutubeForm.js
+++ src/components/YoutubeForm.js
@@ -5,12 +5,18 @@ import TextError from './TextError'
 import PropTypes from 'prop-types'
 
 // the properties in initialValues correspond to the name properties in the form fields
+// the social name/pair shows how to use nested objects when needed and use dot notation to refer to them in the Field component
 const initialValues = {
-    name     : 'Mark',
-    email    : '',
-    channel  : '',
-    comments : '',
-    address  : '',
+    name         : 'Mark',
+    email        : '',
+    channel      : '',
+    comments     : '',
+    address      : '',
+    social       : {
+        facebook : '',
+        twitter  : '',
+    },
+    phoneNumbers : [ '', '' ],
 }
 
 // formiks onSubmit receives the values of the form as its argument
@@ -23,6 +29,11 @@ const validationSchema = Yup.object({
     name    : Yup.string().required('Required'),
     email   : Yup.string().email('Invalid email format').required('Required'),
     channel : Yup.string().required('Required'),
+    address : Yup.string().required('Required'),
+    social  : Yup.object({
+        facebook : Yup.string().required('Required facebook'),
+        twitter  : Yup.string().required('Required twitter'),
+    }),
 })
 
 function YoutubeForm () {
@@ -80,6 +91,28 @@ function YoutubeForm () {
                             )
                         }}
                     </Field>
+                </div>
+
+                <div className='form-control'>
+                    <label htmlFor='facebook'>Facebook profile</label>
+                    <Field type='text' id='facebook' name='social.facebook' />
+                    <ErrorMessage name='social.facebook' component={TextError} />
+                </div>
+
+                <div className='form-control'>
+                    <label htmlFor='twitter'>Twitter profile</label>
+                    <Field type='text' id='twitter' name='social.twitter' />
+                    <ErrorMessage name='social.twitter' component={TextError} />
+                </div>
+
+                <div className='form-control'>
+                    <label htmlFor='primaryPh'>Primary phone number</label>
+                    <Field type='text' id='primaryPh' name='phoneNumbers[0]' />
+                </div>
+
+                <div className='form-control'>
+                    <label htmlFor='secondaryPh'>Secondary phone number</label>
+                    <Field type='text' id='secondaryPh' name='phoneNumbers[1]' />
                 </div>
 
                 <button type='submit'>Submit</button>
