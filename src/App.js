5f54c79f80729e7a33a865f4eecc1a09ec8075d7
Mark Cramer <mecramer@users.noreply.github.com>
2020-09-09 21:43:22 -0400

    Changed controlling component to FormikContainer

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from 'react'
 import './App.css'
 import './components/YoutubeForm'
 import YoutubeForm from './components/YoutubeForm'
+import FormikContainer from './components/FormikContainer'
 
 function App () {
     return (
         <div className='App'>
-            <YoutubeForm />
+            <FormikContainer />
         </div>
     )
 }
