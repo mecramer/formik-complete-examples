bc3c69bdb710114f653c28d1589d9e71b75a9d53
Mark Cramer <mecramer@users.noreply.github.com>
2020-09-09 23:21:10 -0400

    Completed building the test form for all form input types

--- src/components/FormikContainer.js
+++ src/components/FormikContainer.js
@@ -4,13 +4,42 @@ import FormikControl from './FormikControl'
 import * as Yup from 'yup'
 
 function FormikContainer () {
+    const dropdownOptions = [
+        { key: 'Select an option', value: '' },
+        { key: 'Option 1', value: 'option1' },
+        { key: 'Option 2', value: 'option2' },
+        { key: 'Option 3', value: 'option3' },
+    ]
+    const radioOptions = [
+        { key: 'Option 1', value: 'rOption1' },
+        { key: 'Option 2', value: 'rOption2' },
+        { key: 'Option 3', value: 'rOption3' },
+    ]
+    const checkboxOptions = [
+        { key: 'Option 1', value: 'cOption1' },
+        { key: 'Option 2', value: 'cOption2' },
+        { key: 'Option 3', value: 'cOption3' },
+    ]
     const initialValues = {
-        email : '',
+        email          : '',
+        description    : '',
+        selectOption   : '',
+        radioOption    : '',
+        checkboxOption : [],
+        birthDate      : null,
     }
     const validationSchema = Yup.object({
-        email : Yup.string().email('Invalid email format').required('Required'),
+        email          : Yup.string().email('Invalid email format').required('Required'),
+        description    : Yup.string().required('Required'),
+        selectOption   : Yup.string().required('Required'),
+        radioOption    : Yup.string().required('Required'),
+        checkboxOption : Yup.array().required('Required'),
+        birthDate      : Yup.date().required('Required').nullable(),
     })
-    const onSubmit = (values) => console.log('Form data', values)
+    const onSubmit = (values) => {
+        console.log('Form data', values)
+        console.log('Savded data', JSON.parse(JSON.stringify(values)))
+    }
 
     return (
         <Formik
@@ -18,9 +47,29 @@ function FormikContainer () {
             validationSchema={validationSchema}
             onSubmit={onSubmit}
         >
-            {(formik) => (
+            {() => (
                 <Form>
                     <FormikControl control='input' type='email' label='Email' name='email' />
+                    <FormikControl control='textarea' label='Description' name='description' />
+                    <FormikControl
+                        control='select'
+                        label='Select a Topic'
+                        name='selectOption'
+                        options={dropdownOptions}
+                    />
+                    <FormikControl
+                        control='radio'
+                        label='Radio Topic'
+                        name='radioOption'
+                        options={radioOptions}
+                    />
+                    <FormikControl
+                        control='checkbox'
+                        label='Checkbox Topics'
+                        name='checkboxOption'
+                        options={checkboxOptions}
+                    />
+                    <FormikControl control='date' label='Pick a Date' name='birthDate' />
                     <button type='submit'>Submit</button>
                 </Form>
             )}
